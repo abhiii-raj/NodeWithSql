1560d53aa45a715aa963ccc78866bef0d371bcf3
Abhi_Raj <abhishubhraj111@gmail.com>
2025-09-30 10:33:15 +0530

    working with db and node

---
+++ index.js
@@ -0,0 +1,45 @@
+// 1
+const { faker } = require('@faker-js/faker');
+// 4
+const mysql = require('mysql2');
+
+// 5
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'delta_app',
+    password: 'Admin@123'
+});
+
+
+let query = 'SHOW TABLES'; // 10 can be written in a variable and is passed as an argument
+
+let q = 'INSERT INTO user (id, username, email, password) VALUES (?,?,?,?)';
+let user = ["123","Abhi","abhishubhraj@gmail.com","shubhuu"];
+let q1 = 'SELECT * FROM user';
+// 6
+try {
+    connection.query(q1, (err, result) => { 
+        if (err) throw err;
+        console.log(result);  // 8 here result is an array
+        console.log(result.length); // 9
+    });
+}catch(err){
+    console.log(err);
+}
+
+// 7 
+connection.end();
+
+// 2
+let getRandomUser = () => {
+    return {
+        id: faker.string.uuid(),
+        username: faker.internet.username(),
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+    };
+}
+
+// // 3
+// console.log(getRandomUser());
